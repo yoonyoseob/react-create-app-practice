a94edd2c2b2d6687763ad4fafd241fcbb9712e2a
Yoseob Yun <yosub1220@naver.com>
2022-05-25 03:32:10 +0900

    how to use useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import Button from "./Button.js"
-import { useState } from "react";
+import { useState,useEffect } from "react";
 function App() {
   const [value,setValue] = useState(0);
   const onClick = () => setValue((prev) => prev +1);
+  console.log("i run all time ");
+  const iRunOnlyOnce = () =>{
+    console.log('only one time');
+  };
+  useEffect(()=>{
+  console.log("Call the API!")
+  }
+  ,[]);
   return (
+    
+    
+
     <div>
-      <h1>Welcome Back to React!2222</h1>
+      <h1>Welcome Back to React!</h1>
       <h2>{value}</h2>
       <button onClick={onClick}>Click me!</button>
     </div>
