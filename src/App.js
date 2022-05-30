48ac4a01f926bda4f461d90c1c07774329588445
Yoseob Yun <yosub1220@naver.com>
2022-05-30 22:16:50 +0900

    todolist reactjs

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
-import Button from "./Button.js"
-import { useState,useEffect } from "react";
-function App() {
-  const [value,setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev +1);
-  const onChange = (event) => setKeyword(event.target.value);
-  console.log("i run all time ");
-  useEffect(()=>{
-  console.log("Call the API!")
-  }
-  ,[]);
-  useEffect(()=>{
-    if(keyword !== "" && keyword.length > 5){ //if문을 통해서 맨처음에 아무것도 입력하지 않았을때, 실행되는 현상을 고침
-    console.log("Search For" , keyword)};
-  }, [keyword]); // keyword값이 변할때마다, useEffect안에 내용이 실행된다. -> []안에 들어간 값을 트랙킹한다.
+import { useState, useEffect } from 'react';
+function App(){
+  const [todo,setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const onChange = (event) => setTodo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault(); //submit 버튼을 눌렀을때, refresh되는것을 막아준다.
+    if (todo === ""){
+      return; //todo값이 비어있으면 kill
+    }
+    setTodo(""); //add todo 버튼을 눌르고 나면 값을 초기화해준다.
+    setTodos(currentArray => [todo, ...currentArray]); //currentArray -> 값이 추가되기 전 Todos => setTodos를 통해서 현재 입력값인 todo가 추가된다.
+    
+  };
+  console.log(todos); 
   return (
     <div>
-      <input value = {keyword} 
-      onChange={onChange} 
-      type = "text" 
-      placeholder="Search Here.."
-      />
-      <h2>{value}</h2>
-      <button onClick={onClick}>Click me!</button>
+      <h1>My to-do {todos.length}</h1> {/* jsx문법을 통해 todo의 갯수를 보여줌 */}
+      <form onSubmit={onSubmit}>
+        <input  onChange={onChange} value={todo} type="text" placeholder='Write your todo'/>
+        <button>Add Todo</button>
+      </form>
+      <hr />
+      <ul>
+        {todos.map((item,index) => ( //map 함수의 첫 argument는 values, 두번째는 index값을 나타내는 숫자이므로, key값을 정하려면 unique한 값인 index값으로 정하는 것이 마땅하다.
+          <li key={index}>{item}</li> //그냥 text였던 배열이 li로 둘러쌓여서 나오게 된다.
+        ))}
+      </ul>
     </div>
   );
-}
+};
+
 
 export default App;
